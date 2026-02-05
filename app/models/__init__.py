46874de25cac2927e04cc5762a53cc37ac3d7815
Mathieu <mathieu.mjr@gmail.com>
2026-02-05 15:09:20 +0100

    Fixed: import order so SQLAlchemy don't have import issues

---
+++ app/models/__init__.py
@@ -0,0 +1,11 @@
+from .status import Status
+from .audience_type import AudienceType
+from .reservation_type import ReservationType
+from .structure_type import StructureType
+from .audience_type import AudienceType
+from .structure import Structure
+from .audience import Audience
+from .theme import Theme
+
+from .reservation import Reservation
+from .user import User
