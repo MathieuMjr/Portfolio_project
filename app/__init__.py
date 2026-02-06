ac725b10aabd7f6dbdd720bae7aff9925a783318
Mathieu <mathieu.mjr@gmail.com>
2026-02-06 15:00:13 +0100

    Updated: init now use config object

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_restx import Api
 from .extensions import db
+from ..config import DevelopmentConfig
 
 
-def create_app():
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     api = Api(app, version='1.0', title='Sample API',
               description='A sample API using Flask-RESTX')
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+    app.config.from_object(config_class)
     db.init_app(app)
     # namespaces à ajouter ici
     return app
