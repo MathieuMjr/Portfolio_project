6295981102aa1d61b9e05111413de0ae643aab14
Mathieu <mathieu.mjr@gmail.com>
2026-02-10 18:53:51 +0100

    Added: bcrypt + jwt initialization

--- app/extensions.py
+++ app/extensions.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
 
 
 class Base(DeclarativeBase):
@@ -7,3 +9,5 @@ class Base(DeclarativeBase):
 
 
 db = SQLAlchemy(model_class=Base)
+jwt = JWTManager()
+bcrypt = Bcrypt()
