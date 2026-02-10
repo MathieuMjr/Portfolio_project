98cec96abb64f23561e0d4ea96d2274f68a33fce
Mathieu <mathieu.mjr@gmail.com>
2026-02-10 18:53:37 +0100

    Added: bcryt, jwt + namespace

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from flask_restx import Api
-from .extensions import db
+from .extensions import db, jwt, bcrypt
 from config import DevelopmentConfig
+from app.api.users import api as users_ns
 
 
 def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     api = Api(app, version='1.0', title='Sample API',
-              description='A sample API using Flask-RESTX')
+              description='A sample API using Flask-RESTX',
+              doc='/api/')
     app.config.from_object(config_class)
     db.init_app(app)
+    jwt.init_app(app)
+    bcrypt.init_app(app)
+
     # namespaces à ajouter ici
+    api.add_namespace(users_ns, path='/api/users')
+
     return app
