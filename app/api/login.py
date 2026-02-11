8a71e8f8a3a4db7769816c3b2f97229cbe87d7e1
Mathieu <mathieu.mjr@gmail.com>
2026-02-11 17:47:01 +0100

    Created login route with access token creation

---
+++ app/api/login.py
@@ -0,0 +1,28 @@
+from flask_restx import Namespace, Resource
+from app.services import user_service
+from flask_jwt_extended import create_access_token
+
+api = Namespace('login', description='Login route and token creation')
+
+
+@api.route('/')
+class Login(Resource):
+    def post(self):
+        data = api.payload
+        user = user_service.user_repo.get_by_attribute('email', data['email'])
+        if len(user) == 0:
+            return {'error': 'Invalid credentials'}, 401
+        else:
+            res_types_ids = [
+                element.id for element in user[0].reservation_types]
+            if user[0].verify_pwd(data['password']):
+                access_token = create_access_token(
+                    identity=user[0].id,
+                    additional_claims={
+                        'is_admin': user[0].role,
+                        'reservation_types': res_types_ids
+                    }
+                )
+                return {'access_token': access_token}, 200
+            else:
+                return {'error': 'Invalid credentials'}, 401
