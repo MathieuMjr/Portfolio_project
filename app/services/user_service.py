182c0d8f31debca007cf54c66705f1e1a8abb9ad
Mathieu <mathieu.mjr@gmail.com>
2026-02-11 17:49:48 +0100

    Update :  section comments + email uniqueness checking + res_types checking + error handling on res types

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,34 +1,49 @@
 from app.models.user import User
 from app.persistence.user_repository import UserRepository
-from app.persistence.reservation_type_repository import ReservationTypeRepository
+from app.persistence.reservation_type_repository import (
+    ReservationTypeRepository)
 from app.validators.users import UserPayload
-from pydantic import ValidationError
 
 
 class UserServices():
     def __init__(self):
         self.user_repo = UserRepository()
         self.res_type_repo = ReservationTypeRepository()
-# accès au token identity depuis service ? Vérif à faire côté
 
     def create_user(self, payload):
-        try:
-            valid_payload = UserPayload(**payload).model_dump()
-            res_type_data = valid_payload.pop('reservation_types')
+        print('payload in service')
+        # Payload values and type verifications
+        valid_payload = UserPayload(**payload).model_dump()
+        print('payload validated')
 
-            # check admin role
-            # check existing user (via email)
-            new_user = User(**valid_payload)
-            res_types = [
-                self.res_type_repo.get_id(element)
-                for element in res_type_data]
+        # extract reservation_types id
+        res_type_data = valid_payload.pop('reservation_types')
 
-            new_user.reservation_types = res_types
+        # Check email uniqueness:
+        check_user = self.user_repo.get_by_attribute(
+            'email', valid_payload['email'])
+        if len(check_user) != 0:
+            raise LookupError('Email already registered')
 
-            self.user_repo.add(new_user)
+        # Checking and building reservation_types object
+        # to populate relationship:
+        res_types = []
+        for element in res_type_data:
+            resT_obj = self.res_type_repo.get_id(element)
+            if resT_obj is None or not resT_obj.is_active:
+                raise LookupError(f'Reservation_type: {element} not found')
+            else:
+                res_types.append(resT_obj)
 
-            return new_user.to_dict()
-        except ValidationError:
-            raise
-        # except LookupError:
-            # raise LookupError('Email already registered')
+        # User creation:
+        new_user = User(**valid_payload)
+        print('user obj created')
+        new_user.hash_pwd(valid_payload['password'])
+
+        print('res types extracted')
+
+        self.user_repo.add(new_user)
+        # Populate relationship
+        new_user.reservation_types = res_types
+
+        return new_user.to_dict()
