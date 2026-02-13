6dbcf48542b6ed80387e89440ab886fd55299b9d
Mathieu <mathieu.mjr@gmail.com>
2026-02-13 16:56:39 +0100

    Added structure_service instanciation

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,3 +1,5 @@
 from app.services.user_service import UserServices
+from app.services.structure_service import StructureService
 
 user_service = UserServices()
+structure_service = StructureService()
