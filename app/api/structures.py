b667e6e47d41a305b9e49dfb2eea282c12b1f15a
Mathieu <mathieu.mjr@gmail.com>
2026-02-13 16:55:22 +0100

    Created: route for POST new structures

---
+++ app/api/structures.py
@@ -0,0 +1,27 @@
+from flask_restx import Namespace, Resource
+from flask_jwt_extended import (jwt_required)
+from app.services import structure_service
+from pydantic import ValidationError
+
+api = Namespace('structures', description='Structures operations')
+
+
+@api.route('/')
+class Structures(Resource):
+    @jwt_required()
+    def post(self):
+        data = api.payload
+        try:
+            return structure_service.create_struct(data)
+        except ValidationError as e:
+            errors = []
+            for element in e.errors():
+                errors.append({
+                    'field': element['loc'][0],
+                    'value': element['input'],
+                    'msg': element['msg']})
+            return {'error': errors}, 400
+        except LookupError as e:
+            return {'error': str(e)}, 404
+        except ValueError as e:
+            return {'error': str(e)}, 400
