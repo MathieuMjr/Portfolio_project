65c8f47117241da07251ba0f682b3a52fefc8597
Mathieu <mathieu.mjr@gmail.com>
2026-02-13 16:58:51 +0100

    Removed structurecreation class since it is not necessary to have it

--- app/validators/structure.py
+++ app/validators/structure.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel, EmailStr, Field
-from app.models.structure_type import StructureType
 
 
 class StructurePaylaod(BaseModel):
@@ -10,13 +9,3 @@ class StructurePaylaod(BaseModel):
     address: str = Field(min_length=1)
     town: str = Field(min_length=1)
     structure_type_id: str = Field(min_length=1)
-
-
-class StructureCreation(BaseModel):
-    name: str = Field(min_length=1)
-    phone: str = Field(min_length=1)
-    email: EmailStr
-    zip_code: str = Field(min_length=10, max_length=10)
-    address: str = Field(min_length=1)
-    town: str = Field(min_length=1)
-    structure_type: StructureType
