8c51a3291ca54f6b06071b67322b5f0f7c8f26a7
Mathieu <mathieu.mjr@gmail.com>
2026-02-14 19:02:33 +0100

    Created: route to create a new reservation

---
+++ app/api/reservations.py
@@ -0,0 +1,49 @@
+from flask_restx import Namespace, Resource
+from app.services import res_service
+from flask_jwt_extended import (jwt_required,
+                                get_jwt,
+                                get_jwt_identity)
+from pydantic import ValidationError
+from app.services.errors import (UniqueContraintError,
+                                 ThemeDontMatchResType,
+                                 DeactivatedResourceError)
+
+api = Namespace('reservations', description='Reservations operations')
+
+
+@api.route('/')
+class Reservations(Resource):
+    @jwt_required()
+    def post(self):
+        # fetch data
+        data = api.payload
+
+        # extract and check token claims and authorizations
+        identity = get_jwt_identity()
+        claims = get_jwt()
+        role = claims['role']
+        user_res_types = claims['reservation_types']
+
+        if not (role or data['reservation_type_id'] in user_res_types):
+            return {'error': 'Unauthorized action'}, 403
+
+        # add identity to data
+        data['author_id'] = identity
+
+        # create reservation
+        try:
+            return res_service.create_reservation(data)
+        except ValidationError as e:
+            errors = []
+            for element in e.errors():
+                errors.append({
+                    'field': element['loc'][0],
+                    'value': element['input'],
+                    'msg': element['msg']})
+            return {'error': errors}, 400
+        except UniqueContraintError as e:
+            return {'error': str(e)}, 409
+        except ThemeDontMatchResType as e:
+            return {'error': str(e)}, 400
+        except (LookupError, DeactivatedResourceError) as e:
+            return {'error': str(e)}, 404
