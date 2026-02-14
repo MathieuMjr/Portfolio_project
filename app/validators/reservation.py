c900cee60c5f0dbf8871298ae93ecbdd102aeb43
Mathieu <mathieu.mjr@gmail.com>
2026-02-14 18:59:35 +0100

    Removed ReservationCreation + rebaed theme id list for consistency with payload and tests

--- app/validators/reservation.py
+++ app/validators/reservation.py
@@ -2,12 +2,12 @@ from pydantic import BaseModel, Field, field_validator
 from datetime import date
 from datetime import time
 from decimal import Decimal
-from app.models.audience import Audience
-from app.models.status import Status
-from app.models.user import User
-from app.models.theme import Theme
-from app.models.structure import Structure
-from app.models.reservation_type import ReservationType
+# from app.models.audience import Audience
+# from app.models.status import Status
+# from app.models.user import User
+# from app.models.theme import Theme
+# from app.models.structure import Structure
+# from app.models.reservation_type import ReservationType
 
 
 class ReservationPayload(BaseModel):
@@ -26,7 +26,7 @@ class ReservationPayload(BaseModel):
     price: Decimal = Field(gt=0)
     # validateur de price positif
     status_id: str = Field(min_length=1)
-    themes_id: list[str] = Field(min_length=1)
+    themes_id_list: list[str] = Field(min_length=1)
 
     @field_validator('reservation_date')
     def check_date(cls, value):
@@ -35,27 +35,27 @@ class ReservationPayload(BaseModel):
         return value
 
 
-class ReservationCreation(BaseModel):
-    reservation_date: date
-    # ajouter un validateur pour la date
-    # : elle ne doit pas être dans le passé
-    hour: time
-    contact_firstname: str = Field(min_length=1)
-    contact_lastname: str = Field(min_length=1)
-    contact_phone: str = Field(min_length=1)
-    contact_email: str = Field(min_length=1)
-    contact_role: str = Field(min_length=1)
-    price: Decimal = Field(gt=0)
-    # Validateur de price positif
-    author: User
-    themes: list[Theme]
-    audiences: list[Audience]
-    status: Status
-    structure: Structure
-    reservation_type: ReservationType
+# class ReservationCreation(BaseModel):
+#     reservation_date: date
+#     # ajouter un validateur pour la date
+#     # : elle ne doit pas être dans le passé
+#     hour: time
+#     contact_firstname: str = Field(min_length=1)
+#     contact_lastname: str = Field(min_length=1)
+#     contact_phone: str = Field(min_length=1)
+#     contact_email: str = Field(min_length=1)
+#     contact_role: str = Field(min_length=1)
+#     price: Decimal = Field(gt=0)
+#     # Validateur de price positif
+#     author: User
+#     themes: list[Theme]
+#     audiences: list[Audience]
+#     status: Status
+#     structure: Structure
+#     reservation_type: ReservationType
 
-    @field_validator('reservation_date')
-    def check_date(cls, value):
-        if value < date.today():
-            raise ValueError('Date cannot be in the past')
-        return value
+#     @field_validator('reservation_date')
+#     def check_date(cls, value):
+#         if value < date.today():
+#             raise ValueError('Date cannot be in the past')
+#         return value
