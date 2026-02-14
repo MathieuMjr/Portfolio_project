c995c077fee1fda10c9cbd65a252e27930549d30
Mathieu <mathieu.mjr@gmail.com>
2026-02-14 19:01:35 +0100

    Fixe: refactored id and unique contraint checks

--- app/services/structure_service.py
+++ app/services/structure_service.py
@@ -2,6 +2,7 @@ from app.persistence.structure_type_repository import StructureTypeRepository
 from app.persistence.structure_repository import StructureRepository
 from app.models.structure import Structure
 from app.validators.structure import StructurePaylaod
+from app.services.utils import (check_id, check_unique)
 
 
 class StructureService():
@@ -14,24 +15,12 @@ class StructureService():
         valid_payload = StructurePaylaod(**payload).model_dump()
 
         # Check name uniqueness
-        check_obj = self.struct_repo.get_by_attribute(
-            'name', valid_payload['name'])
-        if len(check_obj) != 0 and check_obj[0].is_active is False:
-            raise ValueError(
-                'Deactivated structure with this name already exists')
-        if len(check_obj) != 0 and check_obj[0].is_active is True:
-            raise ValueError('Structure with this name already exist')
+        check_unique(
+            'Structure', 'name', valid_payload['name'], self.struct_repo)
 
         # Check existing structure_type id
         struct_type_id = valid_payload['structure_type_id']
-        check_type = self.structT_repo.get_id(struct_type_id)
-        if not check_type:
-            raise LookupError(
-                f'Structure type {struct_type_id} not found')
-        if check_type.is_active is False:
-            raise LookupError(
-                f'Structure type {valid_payload['name']} is deactivated'
-            )
+        check_id('Structure type', struct_type_id, self.structT_repo)
 
         # Create and save structure
         new_struct = Structure(**valid_payload)
