16ae97f2fd8f4f17267f0457692414edb31cdd9b
Mathieu <mathieu.mjr@gmail.com>
2026-02-14 18:58:15 +0100

    Added: instanciation of reservation service

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,5 +1,7 @@
 from app.services.user_service import UserServices
 from app.services.structure_service import StructureService
+from app.services.reservation_service import ReservationService
 
 user_service = UserServices()
 structure_service = StructureService()
+res_service = ReservationService()
