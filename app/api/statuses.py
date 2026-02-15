39e7366781ce06643dacc766bfc1fabe0e37face
Mathieu <mathieu.mjr@gmail.com>
2026-02-15 23:44:42 +0100

    Created a endpoint to get all statuses

---
+++ app/api/statuses.py
@@ -0,0 +1,13 @@
+from flask_restx import Namespace, Resource
+from flask_jwt_extended import jwt_required
+from app.persistence.status_repository import StatusRepository
+
+api = Namespace('statuses', description='Statuses operations')
+
+
+@api.route('/')
+class Statuses(Resource):
+    @jwt_required()
+    def get(self):
+        status_repo = StatusRepository()
+        return [element.to_dict() for element in status_repo.get_all()]
