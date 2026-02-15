799cf0f1470169ca40eb7ca1f930678db8ab0ee4
Mathieu <mathieu.mjr@gmail.com>
2026-02-15 23:43:38 +0100

    Added: namespaces for status, reservation types, audience types and structure types

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,10 @@ from app.api.users import api as users_ns
 from app.api.login import api as login_ns
 from app.api.structures import api as structures_ns
 from app.api.reservations import api as reservations_ns
+from app.api.struct_types import api as struct_types_ns
+from app.api.statuses import api as statuses_ns
+from app.api.res_types import api as res_types_ns
+from app.api.audience_types import api as audience_types_ns
 
 
 def create_app(config_class=DevelopmentConfig):
@@ -23,5 +27,9 @@ def create_app(config_class=DevelopmentConfig):
     api.add_namespace(login_ns, path='/api/login')
     api.add_namespace(structures_ns, path='/api/structures')
     api.add_namespace(reservations_ns, path='/api/reservations')
+    api.add_namespace(struct_types_ns, path='/api/struct_types')
+    api.add_namespace(statuses_ns, path='/api/statuses')
+    api.add_namespace(res_types_ns, path='/api/res_types')
+    api.add_namespace(audience_types_ns, path='/api/audience_types')
 
     return app
