ee7822875c29cdc5e5c716159102c5ff2b4d619c
Mathieu <mathieu.mjr@gmail.com>
2026-02-15 23:43:52 +0100

    Created: endpoint to get all audience types

---
+++ app/api/audience_types.py
@@ -0,0 +1,13 @@
+from flask_restx import Namespace, Resource
+from flask_jwt_extended import jwt_required
+from app.persistence.audience_type_repository import AudienceTypeRepository
+
+api = Namespace('audience_types', description='Audience types operations')
+
+
+@api.route('/')
+class AudienceTypes(Resource):
+    @jwt_required()
+    def get(self):
+        audienceT_repo = AudienceTypeRepository()
+        return [element.to_dict() for element in audienceT_repo.get_all()]
