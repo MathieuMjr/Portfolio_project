d87a863b79fe8ccb3c40cf53c4cd48a8abfce219
Mathieu <mathieu.mjr@gmail.com>
2026-02-18 19:27:11 +0100

    Added: docstring

--- app/persistence/reservation_repository.py
+++ app/persistence/reservation_repository.py
@@ -10,6 +10,14 @@ class ReservationRepository(Repository):
     # Ici, on précise que le modèle sera User
 
     def user_res_beetween(self, user_id, start_date, end_date):
+        """
+        Query the db to retrieve user's reservation in
+        a date interval (pagination)
+
+        :param user_id: ID of the user
+        :param start_date: Start of the date interval (included)
+        :param end_date: End of the date interval (included)
+        """
         return db.session.scalars(
             db.select(self.model).filter(
                 self.model.author_id == user_id,
