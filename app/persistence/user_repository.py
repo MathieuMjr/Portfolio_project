8d46e501aa5eb818ccade71163b9d20973c58eb5
Mathieu <mathieu.mjr@gmail.com>
2026-02-19 15:28:06 +0100

    Added: a method to save change on a user - used only for password change that can't be done in a repo nor a a route to keep layer responsibilities

--- app/persistence/user_repository.py
+++ app/persistence/user_repository.py
@@ -1,4 +1,4 @@
-from .repository import Repository
+from .repository import Repository, db
 from app.models.user import User
 
 
@@ -9,3 +9,13 @@ class UserRepository(Repository):
     # c'est ce qu'on a mis dans l'init de Repository
     # (self.model = model)
     # Ici, on précise que le modèle sera User
+
+    def save(self, user):
+        """
+        Save a change on a user. Designed for password update in user_service.
+        The goal is to keep layer responsibilities
+        (no hashing in repo, nor in api) and save it on db.
+
+        :param user: Object user
+        """
+        db.session.commit()
