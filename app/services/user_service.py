16c56be7793ee1ac53357339bbc3bdcb52bbf4e3
Mathieu <mathieu.mjr@gmail.com>
2026-02-19 15:28:47 +0100

    Added: method for self user update + route for user update by admin

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -2,7 +2,7 @@ from app.models.user import User
 from app.persistence.user_repository import UserRepository
 from app.persistence.reservation_type_repository import (
     ReservationTypeRepository)
-from app.validators.users import UserPayload, UserUpdate
+from app.validators.users import UserPayload, SelfUpdate, UpdateUserAsAdmin
 from app.services.utils import check_id, check_unique
 
 
@@ -42,27 +42,47 @@ class UserServices():
 
         return new_user.to_dict()
 
+    # --- SELF UPDATE -------------------------------------------------
+
+    def self_update(self, token_identity, data):
+        user = check_id('User', token_identity, self.user_repo)
+        pwd_dict = SelfUpdate(**data).model_dump()
+
+        user.hash_pwd(pwd_dict['password'])
+
+        self.user_repo.save(user)
+
+    #  --- GET A USER BY ID -------------------------------------------
+
     def get_by_id(self, user_id):
         user = self.user_repo.get_id(user_id)
         if not user:
             raise LookupError(f'User {user_id} not found')
         return user.to_dict()
 
-    def put(self, user_id, data):
-
-        valid_data = UserUpdate(**data).model_dump(exclude_unset=True)
+    def patch(self, user_id, data):
+        valid_data = UpdateUserAsAdmin(**data).model_dump(exclude_unset=True)
 
         user = self.user_repo.get_id(user_id)
         if not user:
             raise LookupError(f'User {user_id} not found')
 
-        if 'email' in valid_data:
-            check_unique('User', 'email', valid_data['email'], self.user_repo)
+        if 'reservation_types' in valid_data:
+            res_types = []
+            for reservation in valid_data['reservation_types']:
+                res_type = check_id(
+                    'Reservation', reservation, self.res_type_repo)
+                res_types.append(res_type)
+            user.reservation_types = res_types
+            valid_data.pop('reservation_types')
 
         if 'password' in valid_data:
             user.hash_pwd(valid_data['password'])
             valid_data.pop('password')
-        if user:
+
+        if len(valid_data) == 0:
+            self.user_repo.save(user)
+        else:
             self.user_repo.update(user, valid_data)
 
     def delete(self, user_id):
