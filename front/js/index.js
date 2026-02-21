910791e7816378e83f08dfb1e2ce433c1d543193
Mathieu <mathieu.mjr@gmail.com>
2026-02-21 15:54:17 +0100

    Created a script for index(login page) that fetch on login route

---
+++ front/js/index.js
@@ -0,0 +1,30 @@
+import { getCookie, login } from "./auth.js";
+
+const token = getCookie('token');
+console.log(token);
+
+document.addEventListener('DOMContentLoaded', async () => {
+
+    if (token) {
+        window.location.href = '../html/planning.html';
+    }
+
+    const loginForm = document.querySelector('.login-form');
+    if (loginForm) {console.log('loginForm');}
+    const errorParagraph = document.getElementById('error-credentials');
+
+    loginForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const payload = {
+            'email': document.querySelector('input[name=email]').value,
+            'password': document.querySelector('input[name=pwd]').value
+        }
+        try {
+            const apiToken = await login(payload);
+            document.cookie = "token=" + apiToken.access_token + "; path=/; SameSite=strict";
+            window.location.href = '../html/planning.html';
+        } catch (error) {
+            errorParagraph.textContent = error.message;
+        }
+    });
+});
