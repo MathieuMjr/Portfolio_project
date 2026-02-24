c6f116ef92a7a88c36253ddfdd77d153645264a0
Mathieu <mathieu.mjr@gmail.com>
2026-02-24 18:39:54 +0100

    Fixed: added new field so I can generate audience_type in front

--- app/models/audience_type.py
+++ app/models/audience_type.py
@@ -1,6 +1,7 @@
 from .base import BaseClass
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.schema import UniqueConstraint
 
 
 class AudienceType(BaseClass):
@@ -17,15 +18,33 @@ class AudienceType(BaseClass):
         nullable=False,
         unique=True
     )
+    category: Mapped[str] = mapped_column(
+        String,
+        nullable=False
+    )
+    order_index: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False
+    )
     is_school: Mapped[bool] = mapped_column(
         Boolean,
         nullable=False,
     )
     audiences = relationship('Audience', back_populates='audience_type')
 
+    __table_args__ = (
+        UniqueConstraint(
+            "category",
+            "order_index",
+            name="uq_audience_category_order"
+        ),
+    )
+
     def to_dict(self):
         return {
             "id": self.id,
             "name": self.name,
+            "category": self.category,
+            "order_index": self.order_index,
             "is_school": self.is_school
         }
