183a6a4af5d3c0676eb9b8c74bde7c5d1b3010e5
Mathieu <mathieu.mjr@gmail.com>
2026-02-24 18:40:45 +0100

    Update: new fields added to seeds

--- tests/seeds/seed_audience_types.py
+++ tests/seeds/seed_audience_types.py
@@ -5,23 +5,91 @@ from app.persistence.audience_type_repository import AudienceTypeRepository
 def create_audience_types():
     audienceT_repo = AudienceTypeRepository()
     data = [
-        {'name': 'PS', 'is_school': True},
-        {'name': 'MS', 'is_school': True},
-        {'name': 'GS', 'is_school': True},
-        {'name': 'CP', 'is_school': True},
-        {'name': 'CE1', 'is_school': True},
-        {'name': 'CE2', 'is_school': True},
-        {'name': 'CM1', 'is_school': True},
-        {'name': 'CM2', 'is_school': True},
-        {'name': '6e', 'is_school': True},
-        {'name': '5e', 'is_school': True},
-        {'name': '4e', 'is_school': True},
-        {'name': '3e', 'is_school': True},
-        {'name': 'Seconde', 'is_school': True},
-        {'name': 'Première', 'is_school': True},
-        {'name': 'Terminale', 'is_school': True},
-        {'name': 'Enfants', 'is_school': True},
-        {'name': 'Adultes', 'is_school': True}
+        {
+            'name': 'PS',
+            'category': 'Maternelle',
+            'order_index': 1,
+            'is_school': True},
+        {
+            'name': 'MS',
+            "category": 'Maternelle',
+            'order_index': 2,
+            'is_school': True},
+        {
+            'name': 'GS',
+            "category": 'Maternelle',
+            'order_index': 3,
+            'is_school': True},
+        {
+            'name': 'CP',
+            "category": 'Primaire',
+            'order_index': 1,
+            'is_school': True},
+        {
+            'name': 'CE1',
+            "category": 'Primaire',
+            'order_index': 2,
+            'is_school': True},
+        {
+            'name': 'CE2',
+            "category": 'Primaire',
+            'order_index': 3,
+            'is_school': True},
+        {
+            'name': 'CM1',
+            "category": 'Primaire',
+            'order_index': 4,
+            'is_school': True},
+        {
+            'name': 'CM2',
+            "category": 'Primaire',
+            'order_index': 5,
+            'is_school': True},
+        {
+            'name': '6e',
+            "category": 'Collège',
+            'order_index': 1,
+            'is_school': True},
+        {
+            'name': '5e',
+            "category": 'Collège',
+            'order_index': 2,
+            'is_school': True},
+        {
+            'name': '4e',
+            "category": 'Collège',
+            'order_index': 3,
+            'is_school': True},
+        {
+            'name': '3e',
+            "category": 'Collège',
+            'order_index': 4,
+            'is_school': True},
+        {
+            'name': 'Seconde',
+            "category": 'Lycée',
+            'order_index': 1,
+            'is_school': True},
+        {
+            'name': 'Première',
+            "category": 'Lycée',
+            'order_index': 2,
+            'is_school': True},
+        {
+            'name': 'Terminale',
+            "category": 'Lycée',
+            'order_index': 3,
+            'is_school': True},
+        {
+            'name': 'Enfants',
+            "category": 'Non scolaires',
+            'order_index': 1,
+            'is_school': True},
+        {
+            'name': 'Adultes',
+            "category": 'Non scolaires',
+            'order_index': 2,
+            'is_school': True}
     ]
     for element in data:
         check_element = audienceT_repo.get_by_attribute(
