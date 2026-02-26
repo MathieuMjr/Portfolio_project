ac839697c27856ca5fdc913f98d471ff1020a185
Mathieu <mathieu.mjr@gmail.com>
2026-02-26 19:48:05 +0100

    Update : made two function ; one for fetch statuses, one to fetch themes according to selected reservation type

--- front/js/api.js
+++ front/js/api.js
@@ -1,4 +1,5 @@
 import { API_BASE_URL } from "./config.js";
+import { getCookie } from "./auth.js";
 
 export async function fetchMonthRes(firstDay, lastDay, token) {
     const response = await fetch(
@@ -19,5 +20,44 @@ export async function fetchMonthRes(firstDay, lastDay, token) {
         }
     } else {
         return await response.json();
+    }
+}
+
+export async function fetchStatus() {
+    const token = getCookie("token");
+    const response = await fetch(`${API_BASE_URL}/api/statuses/`, {
+        method: "GET",
+        headers: {
+                "Content-type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        });
+    if (!response.ok) {
+        throw new Error("Une erreur inconnue s'est produite - contacter administrateur");
+    } else {
+        return response.json();
+    }
+}
+
+export async function fetchThemes(resTypeId) {
+    const token = getCookie("token");
+    const response = await fetch(
+        `${API_BASE_URL}/api/res_types/${resTypeId}/themes`, {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        });
+    if (!response.ok) {
+        if (response.status === 400) {
+            throw new Error("Le type de réservation fourni n'a pas été trouvé");
+        } else if (response.status === 401) {
+            throw new Error("Votre session a expiré, reconnectez-vous");
+        } else {
+            throw new Error("Une erreur inconnue s'est produite - contacter administrateur");
+        }
+    } else {
+        return response.json();
     }
 }
