4c4d2fa4b90c775a132d31cbecf990d54d2d3aea
Mathieu <mathieu.mjr@gmail.com>
2026-02-28 17:04:21 +0100

    Added a method to retrieve a list of structures based on a structure type id and a zip code

--- app/persistence/structure_repository.py
+++ app/persistence/structure_repository.py
@@ -1,4 +1,4 @@
-from .repository import Repository
+from .repository import Repository, db
 from app.models.structure import Structure
 
 
@@ -8,3 +8,15 @@ class StructureRepository(Repository):
     # l'ORM a besoin d'un modèle objet pour fonctionner
     # c'est ce qu'on a mis dans l'init de Repository
     # Ici, on précise que le modèle sera User
+
+    def structure_by_type_and_zip(self, structT_id, zip_code):
+        """
+        Query the db to retrieve a list of structure of a given
+        structure type and with a specific zip code.
+        """
+        return db.session.scalars(
+            db.select(self.model).filter(
+                self.model.zip_code == zip_code,
+                self.model.structure_type_id == structT_id
+            )
+        ).all()
