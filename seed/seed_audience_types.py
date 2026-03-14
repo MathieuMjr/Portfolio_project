b16a74270b5b59a0001b8f2481797bfe20fb68a3
Mathieu <mathieu.mjr@gmail.com>
2026-03-14 20:34:25 +0100

    Fixed: order index for a proper display

--- seed/seed_audience_types.py
+++ seed/seed_audience_types.py
@@ -23,72 +23,72 @@ def create_audience_types():
         {
             'name': 'CP',
             "category": 'Primaire',
-            'order_index': 1,
+            'order_index': 4,
             'is_school': True},
         {
             'name': 'CE1',
             "category": 'Primaire',
-            'order_index': 2,
+            'order_index': 5,
             'is_school': True},
         {
             'name': 'CE2',
             "category": 'Primaire',
-            'order_index': 3,
+            'order_index': 6,
             'is_school': True},
         {
             'name': 'CM1',
             "category": 'Primaire',
-            'order_index': 4,
+            'order_index': 7,
             'is_school': True},
         {
             'name': 'CM2',
             "category": 'Primaire',
-            'order_index': 5,
+            'order_index': 8,
             'is_school': True},
         {
             'name': '6e',
             "category": 'Collège',
-            'order_index': 1,
+            'order_index': 9,
             'is_school': True},
         {
             'name': '5e',
             "category": 'Collège',
-            'order_index': 2,
+            'order_index': 10,
             'is_school': True},
         {
             'name': '4e',
             "category": 'Collège',
-            'order_index': 3,
+            'order_index': 11,
             'is_school': True},
         {
             'name': '3e',
             "category": 'Collège',
-            'order_index': 4,
+            'order_index': 12,
             'is_school': True},
         {
             'name': 'Seconde',
             "category": 'Lycée',
-            'order_index': 1,
+            'order_index': 13,
             'is_school': True},
         {
             'name': 'Première',
             "category": 'Lycée',
-            'order_index': 2,
+            'order_index': 14,
             'is_school': True},
         {
             'name': 'Terminale',
             "category": 'Lycée',
-            'order_index': 3,
+            'order_index': 15,
             'is_school': True},
         {
             'name': 'Enfants',
             "category": 'Non scolaires',
-            'order_index': 1,
+            'order_index': 16,
             'is_school': True},
         {
             'name': 'Adultes',
             "category": 'Non scolaires',
-            'order_index': 2,
+            'order_index': 17,
             'is_school': True}
     ]
     for element in data:
